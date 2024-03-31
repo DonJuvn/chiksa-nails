b44013f3724315e981718a62fdb0b4ad15bc7d2c
DonJuan <armvnov1ch@gmail.com>
2024-03-31 23:11:49 +0500

    masters section

--- src/components/masters.jsx
+++ src/components/masters.jsx
@@ -1,8 +1,49 @@
-import React from "react";
-const Masters = () => {
+import React from "react";const Masters = () => {
    return (
       <div id="Masters">
-         <div className="container"></div>
+         <div className="container">
+            <center>
+               <h1 className="title">
+                  <span>Наши Мастерa</span>
+               </h1>
+               <p className="desc">
+                  В Chiksa, мы гордимся нашей командой талантливых и опытных
+                  мастеров маникюра. Каждый из наших специалистов не только
+                  обладает высоким уровнем профессионализма, но и истинной
+                  страстью к своему делу. Встречайте наших звезд:
+               </p>
+            </center>
+            <div className="masters">
+               <div className="master">
+                  <div className="img-bg">
+                     <img src="../../img/master1.svg" alt="" />
+                  </div>
+                  <h3 className="name">Мастер Асель</h3>
+                  <p className="experience">2 года</p>
+               </div>
+               <div className="master">
+                  <div className="img-bg">
+                     <img src="../../img/master2.svg" alt="" />
+                  </div>
+                  <h3 className="name">Мастер Асель</h3>
+                  <p className="experience">2 года</p>
+               </div>
+               <div className="master">
+                  <div className="img-bg">
+                     <img src="../../img/master3.svg" alt="" />
+                  </div>
+                  <h3 className="name">Мастер Асель</h3>
+                  <p className="experience">2 года</p>
+               </div>
+               <div className="master">
+                  <div className="img-bg">
+                     <img src="../../img/master4.svg" alt="" />
+                  </div>
+                  <h3 className="name">Мастер Асель</h3>
+                  <p className="experience">2 года</p>
+               </div>
+            </div>
+         </div>
       </div>
    );
 };
