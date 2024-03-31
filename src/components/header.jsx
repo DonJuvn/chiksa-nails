7cf6888e2849c835a747f35901b7a3aa4d68f894
DonJuan <armvnov1ch@gmail.com>
2024-03-31 18:10:37 +0500

    header navigation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,17 +8,21 @@ const Header = () => {
    return (
       <div id="Header">
          <div className="container">
-            <div className="logo">
-               <img src="../../img/logo.svg" alt="" />
+            <div className="header">
+               <div className="logo">
+                  <img src="../../img/logo.svg" alt="" />
+               </div>
+               <div className="navigation">
+                  <a href="#about">О салоне</a>
+                  <a href="#Masters">Мастера</a>
+                  <a href="#BestWorks">Работы</a>
+                  <a href="#Certificates">Сертификаты</a>
+                  <a href="#Contacts">Контакты</a>
+               </div>
+               <a className="nav-appointment" href="#appointment">
+                  Записаться
+               </a>
             </div>
-            <div className="navigation">
-               <a href="#about">О салоне</a>
-               <a href="#Masters">Мастера</a>
-               <a href="#BestWorks">Лучшие работы</a>
-               <a href="#Certificates">Сертификаты</a>
-               <a href="#Footer">Контакты</a>
-            </div>
-            <a href="#appointment">Записаться</a>
          </div>
       </div>
    );
