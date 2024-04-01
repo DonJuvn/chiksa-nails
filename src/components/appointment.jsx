a8588eaa4d8795b16a28a146c9ecc837f2c5a04f
DonJuan <armvnov1ch@gmail.com>
2024-04-02 02:20:36 +0500

    appointment section skelet

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -1,14 +1,44 @@
-import React from "react";
-
-const Appointment = () => {
+import React from "react";const Appointment = () => {
    return (
-      <div id='appointment'>
+      <div id="appointment">
          <div className="container">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure ullam expedita voluptatibus aspernatur voluptates accusantium cumque sapiente deserunt et maxime rerum, provident quos eius in ad dolore optio alias!</p>
-            <button>ksvnjsdkvnbs</button>
+            <div className="appointment-section">
+               <div className="container">
+                  <h3>
+                     Забронируйте свой визит сегодня и{" "}
+                     <span>позвольте нам позаботиться о вашей красоте</span> с
+                     наивысшим уровнем профессионализма и заботы.
+                  </h3>
+                  <div className="pick-masters">
+                     <button className="picker">Мастер Жанна</button>
+                     <button className="picker">Мастер Асел</button>
+                     <button className="picker">Мастер Айсел</button>
+                     <button className="picker">Мастер Роза</button>
+                     <button className="picker">Мастер Айназ</button>
+                  </div>
+                  <div className="form">
+                     <div className="name">
+                        <p>Имя и фамилия</p>
+                        <input type="text" placeholder=".." />
+                     </div>
+                     <div className="number">
+                        <p>Ваш номер телефона</p>
+                        <input type="text" placeholder=".." />
+                     </div>
+                     <div className="work-type">
+                        <p>Услуга</p>
+                        <input type="text" placeholder="Что хотите сделать?" />
+                     </div>
+                  </div>
+                  <img src="../../img/last.svg" alt="" />
+                  <div className="btn-bg">
+                     <button className="appointment-btn">Записаться</button>
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
-   )
-}
+   );
+};
 
 export default Appointment;
