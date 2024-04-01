ff2a056bf93419c38bd33772ab68645e1cc4bee6
DonJuan <armvnov1ch@gmail.com>
2024-04-02 01:35:47 +0500

    added whatsapp btn

---
+++ src/components/whatsapp.jsx
@@ -0,0 +1,9 @@
+import React from "react";const WhatsAppButton = () => {
+   return (
+      <a id='whatsapp' className="phone" href="#">
+         WhatsApp
+      </a>
+   );
+};
+
+export default WhatsAppButton;
